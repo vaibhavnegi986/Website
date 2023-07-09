8891c2b7b97d5e066c7883b3b3801310a3401f3a
vaibhavnegi986 <vaibhav.negi.986@gmail.com>
2023-07-09 14:56:34 +0530

    First Commit

---
+++ Practice1/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Practice1_Modules;
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Unity;
+using System.Windows;
+
+namespace Practice1
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<AppShell>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+
+        protected override void ConfigureModuleCatalog(IModuleCatalog moduleCatalog)
+        {
+            moduleCatalog.AddModule<StartupModule>();
+        }
+    }
+}
---
+++ Practice1_Modules/StartupModule.cs
@@ -0,0 +1,31 @@
+﻿using Practice1_Modules.ViewModels;
+using Practice1_Modules.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice1_Modules
+{
+    public class StartupModule : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+            //View Register Only
+            var regionManager = containerProvider.Resolve<IRegionManager>();
+            regionManager.RegisterViewWithRegion("LoginRegion", typeof(LoginView));
+            regionManager.RegisterViewWithRegion("DashboardRegion", typeof(DashboardView));
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            //View Register Only
+            containerRegistry.RegisterForNavigation<LoginView, LoginViewModel>();
+            containerRegistry.RegisterForNavigation<DashboardView, DashboardViewModel>();
+        }
+    }
+}
